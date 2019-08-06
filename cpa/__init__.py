ff953b94ca61a3bb530ded6b453f7849da49183e
Florian Ludwig <f.ludwig@greyrook.com>
2019-08-06 21:28:07 +0200

    reformat with black

--- cpa/__init__.py
+++ cpa/__init__.py
@@ -3,5 +3,5 @@
 """Top-level package for Create Python App."""
 
 __author__ = """Florian Ludwig"""
-__email__ = 'f.ludwig@greyrook.com'
-__version__ = '0.1.0'
+__email__ = "f.ludwig@greyrook.com"
+__version__ = "0.1.0"
