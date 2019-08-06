2de2d7992a9f09aa55d19744341c15a9768f4a98
Florian Ludwig <f.ludwig@greyrook.com>
2019-08-06 22:10:32 +0200

    add publishing

--- cpa/cli.py
+++ cpa/cli.py
@@ -5,6 +5,7 @@ import sys
 import os
 import subprocess
 import typing
+import shutil
 
 import click
 import tomlkit
@@ -43,52 +44,7 @@ class Project:
         return self.config["tool"]["cpa"]
 
 
-@click.group()
-def main(args=None):
-    """Console script for cpa."""
-
-    return 0
-
-
-class CommandResult(typing.NamedTuple):
-    output: str
-    returncode: int
-
-
-def run(cmd) -> CommandResult:
-    cmd = ["pipenv", "run"] + cmd
-    proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    return CommandResult(proc.stdout.read(), proc.wait())
-
-
-@main.command()
-def new():
-    """create new project"""
-    raise NotImplementedError()
-
-
-@main.command()
-def update():
-    """update current project"""
-    raise NotImplementedError()
-
-
-@main.command()
-def dist():
-    """create distributables"""
-    raise NotImplementedError()
-
-
-@main.command()
-def publish():
-    """publish to pypi"""
-    raise NotImplementedError()
-
-
-@main.command()
-def test() -> None:
-    """run tests"""
-    project = Project.find()
+def run_tests(project: Project) -> int:
     conf = project.metadata()
     module = conf["name"]
 
@@ -124,6 +80,86 @@ def test() -> None:
         click.echo(mypy_res.output)
         ret_code = ret_code | 4
 
+    return ret_code
+
+
+@click.group()
+def main(args=None):
+    """Console script for cpa."""
+
+    return 0
+
+
+class CommandResult(typing.NamedTuple):
+    output: str
+    returncode: int
+
+
+def run(cmd, capture=True) -> CommandResult:
+    cmd = ["pipenv", "run"] + cmd
+
+    if capture:
+        proc = subprocess.Popen(cmd, stderr=subprocess.STDOUT, stdout=subprocess.PIPE)
+        return CommandResult(proc.stdout.read(), proc.wait())
+    else:
+        proc = subprocess.Popen(cmd)
+        return CommandResult("", proc.wait())
+
+
+@main.command()
+def new():
+    """create new project"""
+    raise NotImplementedError()
+
+
+@main.command()
+def update():
+    """update current project"""
+    raise NotImplementedError()
+
+
+@main.command()
+def dist():
+    """create distributables"""
+    project = Project.find()
+
+    _dist(project)
+
+
+def _dist(project):
+    test_result_code = run_tests(project)
+    if test_result_code != 0:
+        click.secho("Not creating dist due to failing tests", fg="red")
+        sys.exit(test_result_code)
+
+    os.chdir(project.path)
+    if os.path.exists("dist"):
+        shutil.rmtree("dist")
+
+    click.echo(run(["python", "setup.py", "sdist"]).output)
+
+
+@main.command()
+def publish():
+    """publish to pypi"""
+    project = Project.find()
+
+    if not project.metadata().get("public", False):
+        click.secho("Project not public.  Not uploading to pypi", fg="red")
+        sys.exit(1)
+
+    click.secho("Creating distribution")
+    _dist(project)
+    click.secho("Uploading")
+    cmd = ["twine", "upload"] + os.listdir("dist")
+    run(cmd, capture=False)
+
+
+@main.command()
+def test() -> None:
+    """run tests"""
+    project = Project.find()
+    ret_code = run_tests(project)
     sys.exit(ret_code)
 
 
