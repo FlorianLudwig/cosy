ea2c63af994c2f702a470052ca6cef059d7b7e7e
Florian Ludwig <f.ludwig@greyrook.com>
2019-08-25 16:28:44 +0300

    fix setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 with open("README.md") as readme_file:
     readme = readme_file.read()
 
-requirements = ["Click>=6.0"]
+requirements = ["Click>=6.0", "tomlkit", "setuptools"]
 
 setup_requirements = ["pytest-runner"]
 
